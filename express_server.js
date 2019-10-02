0d537e1e13d7de0ef98f10bad4f4d0bfa2004cc9
Lizzyfemme <lizzy_swann@hotmail.com>
2019-10-02 02:20:17 +0000

    login completed

--- express_server.js
+++ express_server.js
@@ -72,6 +72,11 @@ res.cookie("username", req.body.username)
 res.redirect('/urls')
 });
 
+app.post(`/logout`, (req,res) => {
+  res.clearCookie("username")
+  res.redirect('/urls')
+  });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
